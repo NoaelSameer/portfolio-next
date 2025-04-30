6a232dfbc8c4c1320a30e442c1623dc6e1522897
NoaelSameer <142361729+NoaelSameer@users.noreply.github.com>
2025-04-30 14:01:50 -0700

    s

--- src/app/Experiences/page.jsx
+++ src/app/Experiences/page.jsx
@@ -4,14 +4,34 @@ export default function experiences(){
     
     return(
         <>
-        <article>
+        <article className="bg-[#637A9F] h-[95vh]">
         <NavBar Type="About" />
+        <div className="flex justify-between m-5">
+          <div className="h-[80vh] text-white text-center w-[45vw]">
+            <h3 className="text-[7vw]">Education</h3>
+            <div className=" rounded-lg p-6 w-[40vw] text-center">
+                <h4 className="text-xl font-bold mb-2">XXXXXXXX</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <div className=" rounded-lg p-6 w-[40vw] text-center">
+                <h4 className="text-xl font-bold mb-2">XXXXXXXX</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+          </div>
+          <div>
+          <div className="w-[45vw] h-[45vh] mr-[5vw] bg-gray-600 mb-[2vh]" />
+          <button className=" bg-[#E8C872] p-[1vw] px-[2vw] rounded">
+                <a href="###" className="text-black text-[4vw]">FREE! Consultation</a>
+              </button>
+          </div>
+
+        </div>
         </article>
         <article className="h-[100vh] bg-[#004080] text-white">
         <div className="flex justify-center gap-[3vw] pt-[5vh]">
           <div className="w-[45vw] h-[45vh] mr-[5vw] bg-gray-600" />
           <div>
-            <h3 className="text-white text-[3.5vw] mb-[2vh]">Skills & Certs.</h3>
+            <h3 className="text-white text-[3.5vw] mb-[2vh]">Skills & Certs</h3>
             <div className="flex gap-[3vw]">
               <div className="flex flex-col justify-between h-[35vh]">
                 <h4 className="text-[2vw]">Xxxx</h4>
